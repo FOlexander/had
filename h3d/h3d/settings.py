11d5538751722310e33c2a9ed88f049bdd2b4366
Grand4ack <59468903+Grand4ack@users.noreply.github.com>
2022-07-02 21:26:35 +0300

    02/07 settings update

--- h3d/h3d/settings.py
+++ h3d/h3d/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2j@_^1^7777s+emur=9w&cg+j4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
