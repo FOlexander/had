9846efae12a0cbac087037cb0d4da69d9b072944
Grand4ack <59468903+Grand4ack@users.noreply.github.com>
2022-07-04 19:58:02 +0300

    04/07 structure update

--- h3d/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Plot
-# Register your models here.
-
-admin.site.register(Plot)
--- h3d/main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Plot(models.Model):
-    title = models.CharField('Название',max_length=20)
-    plot = models.ImageField('Plot', upload_to='media/',null=True)
-
